94ef8c8c62e7bfd68de8a6538f31f7391b07b014
Maaz Ahmed <elfilosof@protonmail.com>
2024-09-14 21:01:27 -0700

    Init FlowLogParser structure

---
+++ FlowLogParser.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class FlowLogParser {
+    public static void main(String[] args) {
+        String lookupTableFile = "lookup.csv";
+        String flowLogsFile = "flowlog.txt";
+        String outputFile = "output.txt";
+
+        System.out.println("Flow Log Parser started...");
+    }
+}
